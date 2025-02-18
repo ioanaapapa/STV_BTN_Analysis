5d66086f0b09031524bd5da2c2d16f7a089c45c8
ioanapapa <iapapa1@sheffield.ac.uk>
2025-02-18 12:53:10 +0000

    added updated file operations scripts

--- STV_unbound/res_vib/file_concat_all_times.sh
+++ STV_unbound/res_vib/file_concat_all_times.sh
@@ -1,4 +1,4 @@
 for term in {"transvib","rovib"}
 do
-	cat STV_polymer_${term}_50ns.out  STV_polymer_${term}_100ns.out  STV_polymer_${term}_200ns.out STV_polymer_${term}_300ns.out  STV_polymer_${term}_400ns.out > STV_polymer_${term}.out;
+	cat STV_residue_${term}_50ns.out  STV_residue_${term}_100ns.out  STV_residue_${term}_200ns.out STV_residue_${term}_300ns.out  STV_residue_${term}_400ns.out > STV_residue_${term}.out;
 done
